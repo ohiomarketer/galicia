4c709e0b7bc0f18256d1b9db67769436086083f4
ohiomarketer <phonesmarketer@gmail.com>
2024-03-07 02:47:50 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,12 @@ import { Transactions } from "./components/Transactions";
 function App() {
 
     useEffect(() => {
-        // Dynamically change the theme color
-        const metaAppleStatusBar = document.querySelector("meta[name=apple-mobile-web-app-status-bar-style]");
-        metaAppleStatusBar.setAttribute("content", "black"); // Change "black" to your desired color
+    // Dynamically change the status bar color for iOS
+    const metaAppleStatusBar = document.querySelector("meta[name=apple-mobile-web-app-status-bar-style]");
+    if (metaAppleStatusBar) {
+      metaAppleStatusBar.setAttribute("content", "black"); // Change "black" to your desired color
+      console.log("Changed the status bar color for iOS");
+    }
       }, []);
 
   return (
